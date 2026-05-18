d89145cfb178a2ef41c12936e00c60418b86d912
loulousynthcast <louguenskidjy61@gmail.com>
2026-05-17 21:16:45 -0400

    Fix bcrypt backend issue

--- billing/auth.py
+++ billing/auth.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv("JWT_SECRET_KEY", secrets.token_urlsafe(32))
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24 * 7  # 7 days
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto", bcrypt__rounds=12)
 security = HTTPBearer(auto_error=False)
 
 
